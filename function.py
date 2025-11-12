c7c6379b9be55849b36bc68dc33aff608a9fa83b
shafi335 <ms4144776@gmail.com>
2025-11-12 10:21:21 +0500

    Added new files

---
+++ function.py
@@ -0,0 +1,8 @@
+# # Greeting : write a function that takes a name as input and returns a greeting messages
+# vname = input("Enter your name :")
+
+# def greet(name) :
+#     message = f"hell", {name}
+#     print(message)
+
+# greet(vname)
